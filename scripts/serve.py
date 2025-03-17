398194fa00e8c5336087239b0bcd29b080895558
dhchoi-lazy <dhchoi.lazy@gmail.com>
2025-03-17 16:38:23 +0800

    17/03/2025 lecture material

--- scripts/serve.py
+++ scripts/serve.py
@@ -5,7 +5,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from api.server import app
+from src.api.server import app
 
 if __name__ == "__main__":
     host = "0.0.0.0"
