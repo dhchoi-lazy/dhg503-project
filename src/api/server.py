93d780a1852fd6ea71a3e090d6f5ef58959eeba4
dhchoi-lazy <dhchoi.lazy@gmail.com>
2025-03-31 13:03:00 +0800

    aws test

--- src/api/server.py
+++ src/api/server.py
@@ -12,14 +12,7 @@ app.include_router(mqshilu_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*",  # Allow all origins
-        "http://localhost",
-        "http://localhost:80",
-        "http://localhost:5173",
-        "http://ec2-16-163-139-185.ap-east-1.compute.amazonaws.com",
-        "http://ec2-16-163-139-185.ap-east-1.compute.amazonaws.com:80",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
